21e14e6daeb4031583b3f3838bdfb11d9cf94953
Arjun <arjunshandilya05@gmail.com>
2025-10-22 22:59:24 +0530

    Updated Scripts

--- cleaning.py
+++ cleaning.py
@@ -2,10 +2,8 @@ import numpy as np
 import pandas as pd
 
 def ordEnc(df, columns_to_encode):
-
     df_enc = df.copy()
     mappings = {}
-
     for column_name in columns_to_encode:
 
         categories = df_enc[column_name].dropna().unique()
@@ -21,13 +19,9 @@ def ordEnc(df, columns_to_encode):
 
 
 def oneHotEnc(df, columns_to_encode):
-
-    df_enc = df.copy()
-    
+    df_enc = df.copy()  
     for column_name in columns_to_encode:
-
         categories = df[column_name].dropna().unique()
-
         for category in categories:
             df_enc[f"{column_name}_{category}"] = (df_enc[column_name] == category).astype(int)
         
@@ -38,11 +32,8 @@ def oneHotEnc(df, columns_to_encode):
 
 # median and mode can also be used
 def impute(df, columns_to_impute):
-    
     df_imp = df.copy()
-
     for column_name in columns_to_impute:
-
         df_imp[column_name] = df_imp[column_name].fillna(df_imp[column_name].mean())
-
+        
     return df_imp
